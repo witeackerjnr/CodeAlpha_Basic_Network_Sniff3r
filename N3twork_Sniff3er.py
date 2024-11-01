267c2ebb21ad96f307007269bd0b3e739b96daba
witeackerjnr <124600702+witeackerjnr@users.noreply.github.com>
2024-11-01 13:15:08 +0100

    Update N3twork_Sniff3er.py

--- N3twork_Sniff3er.py
+++ N3twork_Sniff3er.py
@@ -1,7 +1,12 @@
 from scapy.all import sniff
+
 def packet_callback(packet):
-    print(packet.summary()) 
-sniff(prn=packet_callback, count=300)
+    print(packet.summary())
 
+def main():
+    print("Starting packet sniffer...")
+    # Start sniffing packets
+    sniff(prn=packet_callback, store=0)
 
-
+if __name__ == "__main__":
+    main()
